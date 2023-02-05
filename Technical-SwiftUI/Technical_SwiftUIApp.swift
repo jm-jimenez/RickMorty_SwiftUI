509f4ea4f10ed25441a86f69687d541ad58e44d9
José María Jiménez <josemariajimenezperez@gmail.com>
2023-02-05 12:12:38 +0100

    Initial Commit

---
+++ Technical-SwiftUI/Technical_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Technical_SwiftUIApp.swift
+//  Technical-SwiftUI
+//
+//  Created by José María Jiménez on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Technical_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
